6bfd4f5ffd20c28e93cd7450f4e0767e795eec9d
rishav76dev <rjrishav037@gmail.com>
2025-03-15 14:41:42 +0530

    integrated be with fr

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
 
-const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+export const Dashboard = () => {
+    return (<div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
+    )
 }
-
-export default Dashboard
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -5,7 +5,7 @@ import { InputBox } from "../components/InputBox"
 import { SubHeading } from "../components/SubHeading"
 
 export const Signin = () => {
-    return <div className="bg-slate-300 h-screen flex justify-center">
+  return (<div className="bg-slate-300 h-screen flex justify-center">
     <div className="flex flex-col justify-center">
       <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
         <Heading label={"Sign in"} />
@@ -19,4 +19,5 @@ export const Signin = () => {
       </div>
     </div>
   </div>
+)
 }
